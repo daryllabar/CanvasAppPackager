8b2787efe0a5dd4ed9a11dfe886a56fa2c3ee728
Daryl LaBar <d3labar@hotmail.com>
2020-02-04 10:49:37 -0500

    Fix for Allow customization Renamed to AllowCustomization #21

--- CanvasAppPackager/Poco/App.cs
+++ CanvasAppPackager/Poco/App.cs
@@ -1,5 +1,4 @@
-﻿using Newtonsoft.Json;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 // ReSharper disable All
 
 namespace CanvasAppPackager.Poco
@@ -165,8 +164,7 @@ namespace CanvasAppPackager.Poco
     {
         public string Description { get; set; }
 
-        [JsonProperty("Allow customization")]
-        public bool AllowCustomizations { get; set; }
+        public bool AllowCustomization { get; set; }
     }
 
     public class TopParent : IControl
